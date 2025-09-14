192fd5975c3ac4d9869ac0be5c252532a8f3c298
lleans <ahmadunnail@gmail.com>
2025-09-14 18:54:51 +0700

    feat: add reset button with tooltip to clear local storage and reload

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,38 @@
+import { RotateCcw } from "lucide-react";
 import { Toaster } from "sonner";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from "./components/animate-ui/radix/tooltip";
 import { DialogStep } from "./components/dialog-step";
 import { ModeToggle } from "./components/mode-toggle";
+import { Button } from "./components/ui/button";
+import { clearAllLocalStorage } from "./lib/utils";
 
 function App() {
   return (
     <>
       <Toaster position="top-right" />
       <ModeToggle />
+      <Tooltip>
+        <TooltipTrigger asChild>
+          <Button
+            size={"icon"}
+            variant="destructive"
+            className="absolute top-16 right-4 z-20"
+            onClick={() => {
+              clearAllLocalStorage();
+              window.location.reload();
+            }}
+          >
+            <RotateCcw />
+          </Button>
+        </TooltipTrigger>
+        <TooltipContent>
+          <p>Reset</p>
+        </TooltipContent>
+      </Tooltip>
       <DialogStep />
     </>
   );
